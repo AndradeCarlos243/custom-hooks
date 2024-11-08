f99d0c7fde123a0994d1b1d06648a2ffa0c67f0b
AndradeCarlos243 <thehamster_gamer@outlook.com>
2024-11-08 17:38:26 -0600

    Reestructuración + todos

---
+++ useTodos/useTodos.js
@@ -0,0 +1,46 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from './todoReducer';
+
+export const useTodos = () => { 
+    const initialState = [];
+    const init = () => JSON.parse(localStorage.getItem('todos')) || [];
+    const [ todos, dispatchTodo ] = useReducer( todoReducer, initialState, init );
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }
+    , [todos]);
+
+    const handleNewTodo = (newTodo) => {
+        const registerTodo = {
+            type: 'add',
+            payload: newTodo
+        }
+        dispatchTodo( registerTodo );
+    }
+
+    const handleDeleteToDo = ( id ) => {
+        const deleteTodo = {
+            type: 'delete',
+            payload: id
+        };
+        dispatchTodo( deleteTodo );
+    }
+
+    const handleToggleToDo = ( id ) => {
+        const toggleTodo = {
+            type: 'toggle',
+            payload: id
+        };
+        dispatchTodo( toggleTodo );
+    }
+
+    return {
+        todos,
+        handleNewTodo,
+        handleDeleteToDo,
+        handleToggleToDo,
+        pendingTotal: todos.filter( todo => !todo.done ).length,
+        todosTotal: todos.length
+    }
+}
